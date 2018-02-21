85674d8d6e0b2ffb1abd165f09a0965ab8b8a65a
David J. H. Shih <djh.shih@gmail.com>
2018-02-21 17:53:11 -0500

    Put in reference C implementation to be translated.

--- src/sample.rs
+++ src/sample.rs
@@ -72,6 +72,92 @@ fn revsort<T: Copy + PartialOrd>(xs: &mut [T], idx: &mut [usize]) {
     }
 }
 
+/*
+/* Unequal probability sampling; with-replacement case */
+
+static void ProbSampleReplace(int n, double *p, int *perm, int nans, int *ans)
+{
+    double rU;
+    int i, j;
+    int nm1 = n - 1;
+
+    /* record element identities */
+    for (i = 0; i < n; i++)
+	perm[i] = i + 1;
+
+    /* sort the probabilities into descending order */
+    revsort(p, perm, n);
+
+    /* compute cumulative probabilities */
+    for (i = 1 ; i < n; i++)
+	p[i] += p[i - 1];
+
+    /* compute the sample */
+    for (i = 0; i < nans; i++) {
+	rU = unif_rand();
+	for (j = 0; j < nm1; j++) {
+	    if (rU <= p[j])
+		break;
+	}
+	ans[i] = perm[j];
+    }
+}
+*/
+
+/*
+void revsort(double *a, int *ib, int n)
+{
+/* Sort a[] into descending order by "heapsort";
+ * sort ib[] alongside;
+ * if initially, ib[] = 1...n, it will contain the permutation finally
+ */
+
+    int l, j, ir, i;
+    double ra;
+    int ii;
+
+    if (n <= 1) return;
+
+    a--; ib--;
+
+    l = (n >> 1) + 1;
+    ir = n;
+
+    for (;;) {
+	if (l > 1) {
+	    l = l - 1;
+	    ra = a[l];
+	    ii = ib[l];
+	}
+	else {
+	    ra = a[ir];
+	    ii = ib[ir];
+	    a[ir] = a[1];
+	    ib[ir] = ib[1];
+	    if (--ir == 1) {
+		a[1] = ra;
+		ib[1] = ii;
+		return;
+	    }
+	}
+	i = l;
+	j = l << 1;
+	while (j <= ir) {
+	    if (j < ir && a[j] > a[j + 1]) ++j;
+	    if (ra > a[j]) {
+		a[i] = a[j];
+		ib[i] = ib[j];
+		j += (i = j);
+	    }
+	    else
+		j = ir + 1;
+	}
+	a[i] = ra;
+	ib[i] = ii;
+    }
+}
+*/
+
 #[cfg(test)]
 mod tests {
     use super::*;
