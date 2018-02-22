5785c9f4edad94611447c383daebd108b3494901
David J. H. Shih <djh.shih@gmail.com>
2018-02-22 00:46:53 -0500

    Added sample functions.

--- src/sample.rs
+++ src/sample.rs
@@ -1,177 +1,169 @@
+use std::cmp;
+
 extern crate rand;
+use sample::rand::distributions::IndependentSample;
 
-/// Sample k from n elements without replacement.
-pub fn subsample(n: usize, k: usize, prob: &[u64], rng: &mut rand::Rng) -> Vec<usize> {
+/// Sample k from n elements with replacement.
+/// prob[i] represents the probability of sampling element i
+pub fn sample(prob: &[f64], k: usize, mut rng: &mut rand::Rng) -> Vec<usize> {
+    let n = prob.len();
 
-    // make indexed probability tuple
-    let mut p: Vec<(u64, usize)> = Vec::new();
-    for i in 0..prob.len() {
-        p[i] = (prob[i], i);
+    if n == 0 {
+        return Vec::new();
+    } else if n == 1 {
+        return vec![0; k];
+    }
+
+    let mut p = indexed_revsorted(&prob);
+
+    // convert probabilities into cumulative probabilities
+    // which collectively represent a roulette
+    for i in 1..n {
+        p[i].0 += p[i-1].0;
+    }
+
+    // draw samples with replacement
+    // i.e. previously drawn samples are not removed
+    let mut ys = Vec::with_capacity(k);
+    let r = rand::distributions::Range::new(0.0, 1.0);
+    let last = n - 1;
+    for _ in 0..k {
+        let u = r.ind_sample(&mut rng);
+        // find index j where random value u lands
+        let mut j = 0;
+        while j < last && u > p[j].0 {
+            j += 1;
+        }
+        ys.push(p[j].1);
     }
    
-    return Vec::new();
-}
-
-/// Sort vector xs by descending order using heapsort,
-/// and sort vector idx by the same permuation.
-/// If idx == 0..n initially, idx will contain the sorted permutation.
-// idx must be the same size as xs
-fn revsort<T: Copy + PartialOrd>(xs: &mut [T], idx: &mut [usize]) {
-    let n = xs.len();
-
-    if n <= 1 {
-        return;
-    }
-    
-    let mut l = n / 2;
-    let mut ir = n - 1;
-
-    let mut ra: T;
-    let mut ii: usize;
-
-    let mut i;
-    let mut j;
-
-    loop {
-        if l > 0 {
-            l = l - 1;
-            ra = xs[l];
-            ii = idx[l];
-        } else {
-            ra = xs[ir];
-            ii = idx[ir];
-
-            xs[ir] = xs[0];
-            idx[ir] = idx[0];
-
-            ir -= 1;
-            if ir == 0 {
-                xs[0] = ra;
-                idx[0] = ii;
-                return;
-            }
-        }
-
-        i = l;
-        j = l / 2;
-        while j <= ir {
-            if j < ir && xs[j] > xs[j + 1] {
-                j += 1;
-            }
-            if ra > xs[j] {
-                xs[i] = xs[j];
-                idx[i] = idx[j];
-                i = j;
-                j += j;
-            } else {
-                j = ir + 1;
-            }
-        }
-        xs[i] = ra;
-        idx[i] = ii;
-    }
+    ys
 }
 
 /*
-/* Unequal probability sampling; with-replacement case */
-
-static void ProbSampleReplace(int n, double *p, int *perm, int nans, int *ans)
+static void ProbSampleNoReplace(int n, double *p, int *perm,
+				int nans, int *ans)
 {
-    double rU;
-    int i, j;
-    int nm1 = n - 1;
+    double rT, mass, totalmass;
+    int i, j, k, n1;
 
-    /* record element identities */
+    /* Record element identities */
     for (i = 0; i < n; i++)
 	perm[i] = i + 1;
 
-    /* sort the probabilities into descending order */
+    /* Sort probabilities into descending order */
+    /* Order element identities in parallel */
     revsort(p, perm, n);
 
-    /* compute cumulative probabilities */
-    for (i = 1 ; i < n; i++)
-	p[i] += p[i - 1];
-
-    /* compute the sample */
-    for (i = 0; i < nans; i++) {
-	rU = unif_rand();
-	for (j = 0; j < nm1; j++) {
-	    if (rU <= p[j])
+    /* Compute the sample */
+    totalmass = 1;
+    for (i = 0, n1 = n-1; i < nans; i++, n1--) {
+	rT = totalmass * unif_rand();
+	mass = 0;
+	for (j = 0; j < n1; j++) {
+	    mass += p[j];
+	    if (rT <= mass)
 		break;
 	}
 	ans[i] = perm[j];
+	totalmass -= p[j];
+	for(k = j; k < n1; k++) {
+	    p[k] = p[k + 1];
+	    perm[k] = perm[k + 1];
+	}
     }
 }
 */
 
-/*
-void revsort(double *a, int *ib, int n)
-{
-/* Sort a[] into descending order by "heapsort";
- * sort ib[] alongside;
- * if initially, ib[] = 1...n, it will contain the permutation finally
- */
+/// Sample k from n elements without replacement.
+/// prob[i] represents the probability of sampling element i
+pub fn subsample(prob: &[f64], k: usize, mut rng: &mut rand::Rng) -> Vec<usize> {
+    let n = prob.len();
 
-    int l, j, ir, i;
-    double ra;
-    int ii;
-
-    if (n <= 1) return;
-
-    a--; ib--;
-
-    l = (n >> 1) + 1;
-    ir = n;
-
-    for (;;) {
-	if (l > 1) {
-	    l = l - 1;
-	    ra = a[l];
-	    ii = ib[l];
-	}
-	else {
-	    ra = a[ir];
-	    ii = ib[ir];
-	    a[ir] = a[1];
-	    ib[ir] = ib[1];
-	    if (--ir == 1) {
-		a[1] = ra;
-		ib[1] = ii;
-		return;
-	    }
-	}
-	i = l;
-	j = l << 1;
-	while (j <= ir) {
-	    if (j < ir && a[j] > a[j + 1]) ++j;
-	    if (ra > a[j]) {
-		a[i] = a[j];
-		ib[i] = ib[j];
-		j += (i = j);
-	    }
-	    else
-		j = ir + 1;
-	}
-	a[i] = ra;
-	ib[i] = ii;
+    if n == 0 {
+        return Vec::new();
+    } else if n == 1 {
+        return vec![0; k];
+    } else if n <= k {
+        return Vec::new();
     }
+
+    let mut p = indexed_revsorted(&prob);
+
+    // draw samples without replacment
+    // i.e. previously drawn samples are removed
+    let mut ys = Vec::with_capacity(k);
+    let r = rand::distributions::Range::new(0.0, 1.0);
+    let mut total = 1.0;
+    let mut last = n - 1;
+    for _ in 0..k {
+        let t = total * r.ind_sample(&mut rng);
+
+        // find index j where random value t lands;
+        // mass needs to be dynamically computed
+        // because p is mutable
+        let mut mass = p[0].0;
+        let mut j = 0;
+        while j < last && t > mass {
+            mass += p[j].0;
+            j += 1;
+        }
+        ys.push(p[j].1);
+
+        // remove element j from the roulette
+        // by shifting elements left
+        for k in j..last {
+            p[k] = p[k+1];
+        }
+        total -= p[j].0;
+        last -= 1;
+    }
+   
+    ys
 }
-*/
+
+/// Sort vector xs by descending order with its index.
+fn indexed_revsorted(xs: &[f64]) -> Vec<(f64, usize)>  {
+    if xs.is_empty() {
+        return Vec::new();
+    }
+
+    // make (probability, index) tuples
+    let mut ts: Vec<(f64, usize)> = Vec::with_capacity(xs.len());
+    for (i, x) in xs.iter().enumerate() {
+        ts.push((*x, i));
+    }
+
+    // sort the proabilities in descending order
+    ts.sort_unstable_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
+
+    ts
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_revsort() {
-        let mut xs = vec![1.0, 5.2, 3.4, 3.3];
-        let mut idx = vec![0, 1, 2, 3];
+    fn test_sort() {
+        let xs = vec![1.0, 5.2, 3.4, 3.3];
+        let ys = indexed_revsorted(&xs);
 
-        revsort(&mut xs, &mut idx);
+        let idx: Vec<usize> = ys.iter().map(|&x| x.1).collect();
 
-        println!("{:?}", xs);
-        println!("{:?}", idx);
+        assert_eq!(idx, vec![1, 2, 3, 0]);
+    }
 
-        assert_eq!(xs, vec![1.0, 3.3, 3.4, 5.2]);
+    #[test]
+    fn test_sample() {
+        let prob = vec![0.5, 0.2, 0.15, 0.1, 0.05];
+        
+        let mut rng = rand::thread_rng();
+
+        let boot = sample(&prob, 10, &mut rng);
+        assert_eq!(boot.len(), 10);
+
+        let sub = subsample(&prob, 4, &mut rng);
+        assert_eq!(sub.len(), 4);
     }
 }
